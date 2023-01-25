e342cf79b1ce38dcc3643658c42aa2d96bed737a
cutraniramlan <cutraniramlan@gmail.com>
2023-01-25 20:49:27 +0100

    update

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -72,6 +72,7 @@ module.exports.getAccessToken = async (event) => {
   );
   // Decode authorization code extracted from the URL query
   const code = decodeURIComponent(`${event.pathParameters.code}`);
+  oAuth2Client.setCredentials({ code });
 
   return new Promise((resolve, reject) => {
     /**
@@ -96,6 +97,61 @@ module.exports.getAccessToken = async (event) => {
         body: JSON.stringify(token),
       };
     })
+    .catch((err) => {
+      // Handle error
+      console.error(err);
+      return {
+        statusCode: 500,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+        },
+        body: JSON.stringify(err),
+      };
+    });
+};
+
+module.exports.getCalendarEvents = async (event) => {
+  // The values used to instantiate the OAuthClient are at the top of the file
+  const oAuth2Client = new google.auth.OAuth2(
+    client_id,
+    client_secret,
+    redirect_uris[0]
+  );
+  // Decode authorization code extracted from the URL query
+  const access_token = decodeURIComponent(
+    `${event.pathParameters.access_token}`
+  );
+  oAuth2Client.setCredentials({ access_token });
+
+  return new Promise((resolve, reject) => {
+    calendar.events.list(
+      {
+        calendarId: calendar_id,
+        auth: oAuth2Client,
+        timeMin: new Date().toISOString(),
+        singleEvents: true,
+        orderBy: "startTime",
+      },
+      (error, response) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(response);
+        }
+      }
+    );
+  })
+
+    .then((results) => {
+      return {
+        statusCode: 200,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+        },
+        body: JSON.stringify({ events: results.data.items }),
+      };
+    })
+
     .catch((err) => {
       // Handle error
       console.error(err);
