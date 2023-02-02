3c11e6f23ba79ae7e409fe923264ccdc2ffdcffb
cutraniramlan <cutraniramlan@gmail.com>
2023-02-02 05:37:26 +0100

    create

---
+++ src/mock-data.js
@@ -0,0 +1,18 @@
+// src/mock-data.js
+
+const mockData = [
+    {
+      "kind": "calendar#event",
+      "etag": ...
+      … 
+    },
+    {
+      "kind": "calendar#event",
+      "etag": ...
+      … 
+    }
+  ];
+  
+  export { mockData };
+
+  mockData = JSON.parse(JSON.stringify(mockData));
