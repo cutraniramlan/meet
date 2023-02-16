9de4f205824f7647661326b2e2876f3556af7ade
cutraniramlan <cutraniramlan@gmail.com>
2023-02-16 09:56:10 +0100

    create and update

--- src/App.js
+++ src/App.js
@@ -5,17 +5,59 @@ import "./App.css";
 import EventList from "./EventList";
 import CitySearch from "./CitySearch";
 import NumberOfEvents from "./NumberOfEvents";
+import { getEvents, extractLocations } from './api';
+import './nprogress.css';
 
 class App extends Component {
+  state = {
+    events: [],
+    locations: [],
+  };
+
   render() {
     return (
       <div className="App">
-        <CitySearch />
-        <EventList />
+        <CitySearch
+          locations={this.state.locations}
+          updateEvents={this.updateEvents}
+        />
+        <EventList events={this.state.events} />
         <NumberOfEvents />
       </div>
     );
   }
 }
 
+updateEvents = (location) => {
+  getEvents().then((events) => {
+    const locationEvents =
+      location === "all"
+        ? events
+        : events.filter((event) => event.location === location);
+    this.setState({
+      events: locationEvents,
+    });
+  });
+};
+
+
 export default App;
+
+componentDidMount() {
+  getEvents().then((events) => {
+    this.setState({ events, locations: extractLocations(events) });
+  });
+}
+
+componentDidMount() {
+  this.mounted = true;
+  getEvents().then((events) => {
+    if (this.mounted) {
+      this.setState({ events, locations: extractLocations(events) });
+    }
+  });
+}
+
+componentWillUnmount(){
+  this.mounted = false;
+}
