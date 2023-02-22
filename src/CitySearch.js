27bab44a56fe16c253845eb606ab262365d6fd28
cutraniramlan <cutraniramlan@gmail.com>
2023-02-22 10:17:24 +0100

    create Alert.js

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,71 +1,5 @@
-/* // src/CitySearch.js
-
 import React, { Component } from "react";
-
-class CitySearch extends Component {
-  state = {
-    query: "",
-    suggestions: [],
-    showSuggestions: undefined,
-  };
-
-  handleInputChanged = (event) => {
-    const value = event.target.value;
-    const suggestions = this.props.locations.filter((location) => {
-      return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
-    });
-    this.setState({
-      query: value,
-      suggestions,
-    });
-  };
-
-  handleItemClicked = (suggestion) => {
-    this.setState({
-      query: suggestion,
-      showSuggestions: false,
-    });
-
-    this.props.updateEvents(suggestion);
-  };
-
-  render() {
-    return (
-      <div className="CitySearch">
-        <input
-          type="text"
-          className="city"
-          value={this.state.query}
-          onChange={this.handleInputChanged}
-          onFocus={() => {
-            this.setState({ showSuggestions: true });
-          }}
-        />
-        <ul
-          className="suggestions"
-          style={this.state.showSuggestions ? {} : { display: "none" }}
-        >
-          {this.state.suggestions.map((suggestion) => (
-            <li
-              key={suggestion}
-              onClick={() => this.handleItemClicked("suggestion")}
-            >
-              {suggestion}
-            </li>
-          ))}
-          <li key="all" onClick={() => this.handleItemClicked("suggestion")}>
-            <b>See all cities</b>
-          </li>
-        </ul>
-      </div>
-    );
-  }
-}
-
-export default CitySearch;
- */
-
-import React, { Component } from "react";
+import { InfoAlert } from "./Alert";
 
 class CitySearch extends Component {
   state = {
@@ -84,13 +18,13 @@ class CitySearch extends Component {
     if (suggestions.length === 0) {
       this.setState({
         query: value,
-        suggestions: [],
-        showSuggestions: false,
+        infoText: "Cannot find that city. Please try again.",
       });
     } else {
       return this.setState({
         query: value,
-        suggestions: suggestions,
+        suggestions,
+        infoText: "",
       });
     }
   };
@@ -100,6 +34,7 @@ class CitySearch extends Component {
       query: suggestion,
       suggestions: [],
       showSuggestions: false,
+      infoText: "",
     });
 
     this.props.updateEvents(suggestion);
@@ -121,6 +56,7 @@ class CitySearch extends Component {
           onChange={this.handleInputChanged}
           onFocus={this.handleInputFocus}
         />
+        <InfoAlert text={this.state.infoText} />
         <ul
           className="suggestions"
           style={this.state.showSuggestions ? {} : { display: "none" }}
@@ -133,9 +69,9 @@ class CitySearch extends Component {
               {suggestion}
             </li>
           ))}
-          <li key="all" onClick={() => this.handleItemClicked("all")}>
-            <h4>List events from all cities</h4>
-          </li>
+          <button key="all" onClick={() => this.handleItemClicked("all")}>
+            List events from all cities
+          </button>
         </ul>
       </div>
     );
