35ef87c86841e519a3e6466fe8eeca21def0d13d
cutraniramlan <cutraniramlan@gmail.com>
2023-02-25 11:25:30 +0100

    create and update

--- src/App.js
+++ src/App.js
@@ -5,33 +5,42 @@ import "./App.css";
 import EventList from "./EventList";
 import CitySearch from "./CitySearch";
 import NumberOfEvents from "./NumberOfEvents";
-import { getEvents, extractLocations } from "./api";
 import "./nprogress.css";
 import { InfoAlert } from "./Alert";
+import WelcomeScreen from "./WelcomeScreen";
+import { getEvents, extractLocations, checkToken, getAccessToken } from "./api";
 
 class App extends Component {
   state = {
     events: [],
     locations: [],
+    showWelcomeScreen: undefined,
   };
 
   render() {
-    return (
-      <div className="App">
-        {!navigator.onLine && (
-          <InfoAlert
-            className="alert-centered"
-            text="App is currently offline. You are seeing your cached data."
+    if (this.state.showWelcomeScreen === undefined)
+      return (
+        <div className="App">
+          {!navigator.onLine && (
+            <InfoAlert
+              className="alert-centered"
+              text="App is currently offline. You are seeing your cached data."
+            />
+          )}
+          <CitySearch
+            locations={this.state.locations}
+            updateEvents={this.updateEvents}
           />
-        )}
-        <CitySearch
-          locations={this.state.locations}
-          updateEvents={this.updateEvents}
-        />
-        <NumberOfEvents />
-        <EventList events={this.state.events} />
-      </div>
-    );
+          <NumberOfEvents />
+          <EventList events={this.state.events} />
+          <WelcomeScreen
+            showWelcomeScreen={this.state.showWelcomeScreen}
+            getAccessToken={() => {
+              getAccessToken();
+            }}
+          />
+        </div>
+      );
   }
 
   updateEvents = (location) => {
@@ -46,13 +55,20 @@ class App extends Component {
     });
   };
 
-  componentDidMount() {
+  async componentDidMount() {
     this.mounted = true;
-    getEvents().then((events) => {
-      if (this.mounted) {
-        this.setState({ events, locations: extractLocations(events) });
-      }
-    });
+    const accessToken = localStorage.getItem("access_token");
+    const isTokenValid = (await checkToken(accessToken)).error ? false : true;
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = searchParams.get("code");
+    this.setState({ showWelcomeScreen: !(code || isTokenValid) });
+    if ((code || isTokenValid) && this.mounted) {
+      getEvents().then((events) => {
+        if (this.mounted) {
+          this.setState({ events, locations: extractLocations(events) });
+        }
+      });
+    }
   }
 
   componentWillUnmount() {
