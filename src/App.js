21ddb357ae9611d2a39cc74cb4648ec36b5dadf1
cutraniramlan <cutraniramlan@gmail.com>
2023-03-18 11:24:00 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -147,24 +147,23 @@ class App extends Component {
         {!showWelcomeScreen && (
           <div className="data-vis-wrapper">
             <EventGenre className="mb-20" events={events} />
-            <ResponsiveContainer height={400}>
-              <ScatterChart
-                width={400}
-                height={400}
-                margin={{
-                  top: 20,
-                  right: 20,
-                  bottom: 20,
-                  left: 20,
-                }}
-              >
-                <CartesianGrid />
-                <XAxis type="category" dataKey="city" name="city" />
-                <YAxis type="number" dataKey="number" name="number of events" />
-                <Tooltip cursor={{ strokeDasharray: "3 3" }} />
-                <Scatter data={this.getData()} fill="#8884d8" />
-              </ScatterChart>
-            </ResponsiveContainer>
+
+            <ScatterChart
+              width={400}
+              height={400}
+              margin={{
+                top: 20,
+                right: 20,
+                bottom: 20,
+                left: 20,
+              }}
+            >
+              <CartesianGrid />
+              <XAxis type="category" dataKey="city" name="city" />
+              <YAxis type="number" dataKey="number" name="number of events" />
+              <Tooltip cursor={{ strokeDasharray: "3 3" }} />
+              <Scatter data={this.getData()} fill="#8884d8" />
+            </ScatterChart>
           </div>
         )}
         <div className="w-full flex justify-center mt-4">
